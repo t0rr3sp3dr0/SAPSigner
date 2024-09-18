3d987f5d3a8694e820bed509bd604a53bfda180e
Pedro Tôrres <t0rr3sp3dr0@gmail.com>
2024-09-17 23:15:49 -0700

    format tests
    Signed-off-by: Pedro Tôrres <t0rr3sp3dr0@gmail.com>

--- hack/test-authenticate.sh
+++ hack/test-authenticate.sh
@@ -1,16 +1,56 @@
 #!/bin/bash
 set -eux -o 'pipefail'
 
-DIR="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
-SIG="$(: | DYLD_LIBRARY_PATH="${DIR}/../lib" LD_LIBRARY_PATH="${DIR}/../lib" "${1}" | base64 | tr -d '\n')"
+SAPSIGNER="${1}"
+function sapsigner {
+	local SRC="$(readlink -f -- "${BASH_SOURCE[0]}")"
+	local DIR="$(dirname -- "${SRC}")"
 
-: \
-| curl \
-    -H 'User-Agent: MacAppStore/1.3 (Macintosh; OS X 10.9.5) AppleWebKit/537.78.2' \
-    -H "X-Apple-ActionSignature: ${SIG}" \
-    -X 'POST' \
-    -f \
-    -o '/dev/null' \
-    -w '%{response_code}\n' \
-    'https://p40-buy.itunes.apple.com/WebObjects/MZFinance.woa/wa/authenticate' \
-    2> '/dev/null'
+	DYLD_LIBRARY_PATH="${DIR}/../lib" LD_LIBRARY_PATH="${DIR}/../lib" "${SAPSIGNER}" "${@:+"${@}"}"
+}
+
+DAT="$(
+	cat <<- EOF
+		<?xml version="1.0" encoding="UTF-8"?>
+		<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+		<plist version="1.0">
+		    <dict>
+		        <key>appleId</key>
+		        <string>***</string>
+		        <key>attempt</key>
+		        <string>2</string>
+		        <key>guid</key>
+		        <string>***</string>
+		        <key>password</key>
+		        <string>***</string>
+		        <key>rmp</key>
+		        <string>0</string>
+		        <key>why</key>
+		        <string>signIn</string>
+		    </dict>
+		</plist>
+	EOF
+)"
+
+SIG="$(
+	:                \
+	| echo           \
+		-n       \
+		"${DAT}" \
+	| sapsigner      \
+	| base64         \
+	;
+)"
+
+:                                                                                      \
+| curl                                                                                 \
+	-H 'User-Agent: MacAppStore/1.3 (Macintosh; OS X 10.9.5) AppleWebKit/537.78.2' \
+	-H "X-Apple-ActionSignature: ${SIG}"                                           \
+	-X 'POST'                                                                      \
+	-d "${DAT}"                                                                    \
+	-f                                                                             \
+	-o '/dev/null'                                                                 \
+	-w '%{response_code}\n'                                                        \
+	'https://p40-buy.itunes.apple.com/WebObjects/MZFinance.woa/wa/authenticate'    \
+	2> '/dev/null'                                                                 \
+;
--- hack/test-signupWizard.sh
+++ hack/test-signupWizard.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 set -eux -o 'pipefail'
 
-DIR="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
-SIG="$(: | DYLD_LIBRARY_PATH="${DIR}/../lib" LD_LIBRARY_PATH="${DIR}/../lib" "${1}" -p | base64 | tr -d '\n')"
+SAPSIGNER="${1}"
+function sapsigner {
+	local SRC="$(readlink -f -- "${BASH_SOURCE[0]}")"
+	local DIR="$(dirname -- "${SRC}")"
 
-: \
-| curl \
-    -H 'User-Agent: iTunes/12.6.2 (Macintosh; OS X 10.9.5) AppleWebKit/537.78.2' \
-    -H "X-Apple-ActionSignature: ${SIG}" \
-    -X 'GET' \
-    -f \
-    -o '/dev/null' \
-    -w '%header{X-Apple-ActionSignature}\n' \
-    'https://p49-buy.itunes.apple.com/WebObjects/MZFinance.woa/wa/signupWizard?guid=000000000000' \
-    2> '/dev/null' \
-| grep \
-    -E \
-    '^([0-9A-Za-z+/]{4})*([0-9A-Za-z+/]{4}|[0-9A-Za-z+/]{3}=|[0-9A-Za-z+/]{2}==)$'
+	DYLD_LIBRARY_PATH="${DIR}/../lib" LD_LIBRARY_PATH="${DIR}/../lib" "${SAPSIGNER}" "${@:+"${@}"}"
+}
+
+SIG="$(
+	:           \
+	| sapsigner \
+		-p  \
+	| base64    \
+	;
+)"
+
+:                                                                                                     \
+| curl                                                                                                \
+	-H 'User-Agent: iTunes/12.6.2 (Macintosh; OS X 10.9.5) AppleWebKit/537.78.2'                  \
+	-H "X-Apple-ActionSignature: ${SIG}"                                                          \
+	-X 'GET'                                                                                      \
+	-f                                                                                            \
+	-o '/dev/null'                                                                                \
+	-w '%header{X-Apple-ActionSignature}\n'                                                       \
+	'https://p49-buy.itunes.apple.com/WebObjects/MZFinance.woa/wa/signupWizard?guid=000000000000' \
+	2> '/dev/null'                                                                                \
+| grep                                                                                                \
+	-E                                                                                            \
+	'^([0-9A-Za-z+/]{4})*([0-9A-Za-z+/]{4}|[0-9A-Za-z+/]{3}=|[0-9A-Za-z+/]{2}==)$'                \
+;
